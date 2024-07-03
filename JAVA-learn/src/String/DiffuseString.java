7f87f1d40a6064309181e31c81ddae064736472e
wsy <u202111340@hust.edu.cn>
2024-07-03 20:08:45 +0800

    update

---
+++ JAVA-learn/src/String/DiffuseString.java
@@ -0,0 +1,21 @@
+package String;
+
+import java.util.Random;
+
+public class DiffuseString {
+    public static void main(String[] args) {
+        String str = "1145141919810";
+        char[] chs = str.toCharArray();
+        Random r = new Random();
+        for (int i = 0; i < chs.length; i++) {
+            swap(chs,i, r.nextInt(chs.length));
+        }
+        String res = new String(chs);
+        System.out.println(res);
+    }
+    public static void swap(char[] arr,int i,int j){
+        char temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
