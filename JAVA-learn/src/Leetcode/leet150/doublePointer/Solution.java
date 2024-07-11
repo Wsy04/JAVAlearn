6c73de4a02320ec02c9691a00df1fcb8df6a5fda
wsy <u202111340@hust.edu.cn>
2024-07-11 12:08:42 +0800

    update

---
+++ JAVA-learn/src/Leetcode/leet150/doublePointer/Solution.java
@@ -0,0 +1,28 @@
+package Leetcode.leet150.doublePointer;
+
+public class Solution {
+    public static void main(String[] args) {
+        String s = "A man, a plan, a canal: Panama";
+        System.out.println(isPalindrome(s));
+    }
+
+    public static boolean isPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
+        while (left < right) {
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+            if (left < right) {
+                if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                    return false;
+                }
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
